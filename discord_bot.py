ae98cd6c50b522f6768275496054c8f4c26fe5f3
Nelson Pang <nelsoncpang@gmail.com>
2023-09-11 19:15:55 -0700

    added some comments

--- discord_bot.py
+++ discord_bot.py
@@ -2,32 +2,28 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os 
-import requests
 from yelpapi import YelpAPI
-
+import random
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN") 
 YELP_KEY = os.getenv("YELP_KEY")
 
-
 intents = discord.Intents.default()
 intents.message_content = True
 
-# client = discord.Client(intents=intents)
-
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
+# Restaurants
 @bot.command()
-async def restaurant(ctx, city):
+async def res(ctx, *, city):
     try:
         yelp_api = YelpAPI(YELP_KEY)
-        response = yelp_api.search_query(location=city, term='restaurant', limit=5)
+        response = yelp_api.search_query(location=city, term='restaurant',  sort_by='rating', limit=5)
        
         for business in response['businesses']:
-            await ctx.send(f"Name: {business['name']}\nRating: {business['rating']}\nAddress: {', '.join(business['location']['display_address'])}\n")
+            await ctx.send(f"{business['name']}\nRating: {business['rating']}\nAddress: {', '.join(business['location']['display_address'])}\n-------------------------------------")
     except Exception as e:
         print(f"An error occurred: {e}")
         await ctx.send(f"Error: {e}")
